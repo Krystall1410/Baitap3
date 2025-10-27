0435c9e0382d3a75461167a5fd2295a1757307e6
Krystall1410 <sonnas1410@gmail.com>
2025-10-27 13:20:09 +0700

    fix

--- php/login/process_login.php
+++ php/login/process_login.php
@@ -57,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             header("Location: shop.html");
                             exit;
                         }
-                    }
+                    }   
                 }
             }
         }
